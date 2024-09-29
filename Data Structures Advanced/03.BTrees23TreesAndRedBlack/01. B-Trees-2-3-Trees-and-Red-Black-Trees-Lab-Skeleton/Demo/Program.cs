baa3848edcb58cb81288dc8003741cc6f6568fb6
Borislav16 <borko.g16@gmail.com>
2024-09-29 21:40:53 +0300

    Create Program.cs

---
+++ Data Structures Advanced/03.BTrees23TreesAndRedBlack/01. B-Trees-2-3-Trees-and-Red-Black-Trees-Lab-Skeleton/Demo/Program.cs
@@ -0,0 +1,23 @@
+﻿using _01.Two_Three;
+using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main()
+        {
+            var tree = new TwoThreeTree<string>();
+
+            tree.Insert("D");
+            tree.Insert("G");
+            tree.Insert("A");
+            tree.Insert("P");
+            tree.Insert("Z");
+            tree.Insert("F");
+            tree.Insert("E");
+
+            Console.WriteLine(tree.ToString());
+        }
+    }
+}
