198661987df227fde54c85546fde8162442904ba
Borislav16 <borko.g16@gmail.com>
2024-09-30 19:56:19 +0300

    Create Program.cs

---
+++ Data Structures Advanced/04.ExerciseBTrees23TreesAndRedBlack/02. B-Trees-2-3-Trees-and-Red-Black-Trees-Exercise-Skeleton/Demo/Program.cs
@@ -0,0 +1,20 @@
+﻿using _01.RedBlackTree;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main()
+        {
+            var rbt = new RedBlackTree<int>();
+
+            rbt.Insert(12);
+            rbt.Insert(3);
+            rbt.Insert(5);
+            rbt.Insert(8);
+            rbt.Insert(32);
+            rbt.Insert(45);
+            rbt.Insert(1);
+        }
+    }
+}
