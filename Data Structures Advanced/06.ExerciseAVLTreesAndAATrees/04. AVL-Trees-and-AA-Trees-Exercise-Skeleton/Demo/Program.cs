b3f51ebef01fcb484421206afa1f389e6711f45d
Borislav16 <borko.g16@gmail.com>
2024-10-03 20:54:07 +0300

    Create Program.cs

---
+++ Data Structures Advanced/06.ExerciseAVLTreesAndAATrees/04. AVL-Trees-and-AA-Trees-Exercise-Skeleton/Demo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AVLTree;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main()
+        {
+            var tree = new AVL<int>();
+
+            tree.Insert(12);
+            tree.Insert(3);
+            tree.Insert(5);
+            tree.Insert(8);
+            tree.Insert(35);
+            tree.Insert(16);
+            tree.Insert(1);
+
+        }
+    }
+}
