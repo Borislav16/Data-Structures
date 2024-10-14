d93a73cb901002c9c54b8fc20c6c2ae62feabea4
Borislav16 <borko.g16@gmail.com>
2024-10-14 17:55:48 +0300

    Create Message.cs

---
+++ Data Structures Advanced/10.ExamPreparation/Exam.Discord/Message.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Exam.Discord
+{
+    public class Message
+    {
+        public Message(string id, string content, int timestamp, string channel)
+        {
+            Id = id;
+            Content = content;
+            Timestamp = timestamp;
+            Channel = channel;
+        }
+
+        public string Id { get; set; }
+
+        public string Content { get;set; }
+
+        public int Timestamp { get; set; }
+
+        public string Channel { get; set; }
+
+        public List<string> Reactions { get; set; } = new List<string>();
+    }
+}
