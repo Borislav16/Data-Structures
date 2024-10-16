4e0c98bb418b14c1418a7d6f2b1ec762b169401d
Borislav16 <borko.g16@gmail.com>
2024-10-16 20:59:33 +0300

    Create Route.cs

---
+++ Data Structures Advanced/10.ExamPreparation/Exam.MoovIt/Route.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Exam.MoovIt
+{
+    public class Route
+    {
+        public string Id { get; set; }
+
+        public double Distance { get; set; }
+
+        public int Popularity { get; set; }
+
+        public bool IsFavorite { get; set; }
+
+        public List<string> LocationPoints { get; set; } = new List<string>();
+
+        public Route(string id, double distance, int popularity, bool isFavorite, List<string> locationPoints)
+        {
+            Id = id;
+            Distance = distance;
+            Popularity = popularity;
+            IsFavorite = isFavorite;
+            LocationPoints = locationPoints;
+        }
+    }
+}
