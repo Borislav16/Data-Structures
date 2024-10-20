cf37140cd8186cdd1a509615fad25c5b40e6085f
Borislav16 <borko.g16@gmail.com>
2024-10-21 00:55:12 +0300

    Create Node.cs

---
+++ Data Structures Fundamentals/04.LinearDataStructures/Problem03.Queue/Node.cs
@@ -0,0 +1,32 @@
+﻿namespace Problem03.Queue
+{
+    public class Node<T>
+    {
+        public T Value;
+        public Node<T> Next;
+        public Node<T> Prev;
+
+        public Node()
+        {
+        }
+
+        public Node(T value)
+        {
+            Value = value;
+        }
+
+        public Node(T value, Node<T> next)
+            : this(value)
+        {
+            Next = next;
+        }
+
+        public Node(T value, Node<T> next, Node<T> prev)
+            : this(value, next)
+        {
+            Prev = prev;
+        }
+
+
+    }
+}
