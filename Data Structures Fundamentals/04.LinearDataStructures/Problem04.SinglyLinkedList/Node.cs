850e93dadb171ab54c5f452908c0ed4944ce4054
Borislav16 <borko.g16@gmail.com>
2024-10-21 18:56:12 +0300

    Create Node.cs

---
+++ Data Structures Fundamentals/04.LinearDataStructures/Problem04.SinglyLinkedList/Node.cs
@@ -0,0 +1,30 @@
+﻿namespace Problem04.SinglyLinkedList
+{
+    public class Node<T>
+    {
+        public T Value;
+        public Node<T> Next;
+        public Node<T> Prev;
+
+        public Node()
+        {
+        }
+
+        public Node(T value)
+        {
+            Value = value;
+        }
+
+        public Node(T value, Node<T> next)
+            : this(value)
+        {
+            Next = next;
+        }
+
+        public Node(T value, Node<T> next, Node<T> prev)
+            : this(value, next)
+        {
+            Prev = prev;
+        }
+    }
+}
