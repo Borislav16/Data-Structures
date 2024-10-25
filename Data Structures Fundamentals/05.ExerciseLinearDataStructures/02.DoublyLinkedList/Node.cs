5004ba965f72c535ebd1be83931339a1c887938e
Borislav16 <borko.g16@gmail.com>
2024-10-25 13:46:22 +0300

    Create Node.cs

---
+++ Data Structures Fundamentals/05.ExerciseLinearDataStructures/02.DoublyLinkedList/Node.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Linq;
+
+namespace Problem02.DoublyLinkedList
+{
+    public class Node<T>
+    {
+        public Node()
+        {
+            
+        }
+
+
+        public Node<T> Next { get; set; }
+
+        public Node<T> Previous { get; set; }
+
+        public T Value { get; set; }
+
+
+    }
+}
