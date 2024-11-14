9bc0455fb356d265e7c044a5aba023a7769d0753
Borislav16 <borko.g16@gmail.com>
2024-11-14 20:37:54 +0200

    Create CookiesProblem.cs

---
+++ Data Structures Fundamentals/09.ExerciseHeapsAndBST/04.CookiesProblem/CookiesProblem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using _03.MinHeap;
+using Wintellect.PowerCollections;
+
+namespace _04.CookiesProblem
+{
+    public class CookiesProblem
+    {
+        public int Solve(int minSweetness, int[] cookies)
+        {
+            OrderedBag<int> cookieBag = new OrderedBag<int>();
+
+            cookieBag.AddMany(cookies);
+
+            int steps = 0;
+            int currentSweetness = cookieBag.GetFirst();
+
+            while(currentSweetness < minSweetness && cookieBag.Count > 1)
+            {
+                int firstCookie = cookieBag.RemoveFirst();
+                int secondCookie = cookieBag.RemoveFirst();
+                
+                int newCookie = firstCookie + 2 * secondCookie;
+                cookieBag.Add(newCookie);
+                currentSweetness = cookieBag.GetFirst();
+
+                steps++;
+            }
+
+            return currentSweetness < minSweetness ? -1 : steps;
+        }
+    }
+}
